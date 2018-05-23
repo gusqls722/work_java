cc1350c5cad6ababc39c455d3c5952ecc49fa3e2
KOITT <KOITT@KOITT-PC>
2018-05-23 21:28:34 +0900

    2장 34p 예제까지 완료

---
+++ ch02/src/ch02/VarEx3.java
@@ -0,0 +1,19 @@
+package ch02;
+
+public class VarEx3 {
+	final static int MAX_SPEED =10;		//MAX_SPEED는 상수, 10은 리터럴
+	final static int WIDTH =10;			//삼각형의 밑변
+	final static int HEIGHT =2;			//삼각형의 높이
+	/*
+	 * final이 붙으면 앞으로 MAX_SPEED는
+	 * 프로그램이 종료될 때 까지 10으로 고정
+	 */
+	
+	public static void main(String[] args){
+		//MAX_SPEED = MAX_SPEED + 10; 상수는 값을 변경 할 수 없다.
+		System.out.println(MAX_SPEED);	//상수 MAX_SPEED 값을 출력
+		
+		int TriAngleArea = WIDTH * HEIGHT / 2;
+		System.out.printf("삼각형의 넓이: %d " , TriAngleArea);
+	}
+}
