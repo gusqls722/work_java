48f35df20028225fc2eff433c3c0d0d2663ad1da
KOITT <KOITT@KOITT-PC>
2018-05-26 18:00:26 +0900

    05-26 2장 81쪽 형변환까지 완료

---
+++ ch02/src/ch02/CastingEx1.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class CastingEx1 {
+	static public void main(String[] args) {
+		double d= 85.4;
+		int score = (int)d;
+		String a = "";
+		
+		
+		System.out.println("score = "+score);
+		System.out.println("d = "+d);
+	
+	}
+}
---
+++ ch02/src/ch02/CastingEx5.java
@@ -0,0 +1,17 @@
+package ch02;
+
+public class CastingEx5 {
+	public static void main(String[] args) {
+		float f = 1234; 		// 형변환의 생략. float f= (float)1234;와 같음
+		byte b= (byte)1000; 			// 에러.byte의 범위(-128~127)를 넘는 값을 저장.
+		char ch = (char)1000;	//명시적 형변환. 에러가 발생하지 않음
+		int i = 3;
+		double d = 1.0 + i;		//double d = 1.0 + (double)i;에서 형변환이 생략됨
+		
+		System.out.println(ch);
+		/*
+		 * -자동 형변환의 규칙-
+		 * byte -> short(char) -> int -> long -> float -> double
+		 */
+	}
+}
