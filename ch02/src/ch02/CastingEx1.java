3d38e060a23682562a62a96573e5cef50fc06e5d
KOITT <KOITT@KOITT-PC>
2018-05-26 18:02:58 +0900

    0526 2장 81쪽 형변환까지 완료

--- ch02/src/ch02/CastingEx1.java
+++ ch02/src/ch02/CastingEx1.java
@@ -4,11 +4,8 @@ public class CastingEx1 {
 	static public void main(String[] args) {
 		double d= 85.4;
 		int score = (int)d;
-		String a = "";
-		
 		
 		System.out.println("score = "+score);
 		System.out.println("d = "+d);
-	
 	}
 }
