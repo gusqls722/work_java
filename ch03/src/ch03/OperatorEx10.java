edb1aec5d4ed953e4e98ace43d1d18b9afec928b
KOITT <KOITT@KOITT-PC>
2018-05-28 21:39:23 +0900

    05-28
    3장 101쪽 예제 10번까지 완료

--- ch03/src/ch03/OperatorEx10.java
+++ ch03/src/ch03/OperatorEx10.java
@@ -5,6 +5,9 @@ public class OperatorEx10 {
 		int a = 1000000;
 		
 		int result1 = a * a / a;	// 1000000 * 1000000 / 1000000
+		/*long result1 = (long)a * a / a;
+		 * 순서를 바꾸기 싫다면 롱타입으로 위처럼 형변환시킨다
+		 */
 		int result2 = a / a * a;	// 1000000 * 1000000 / 1000000
 		
 		System.out.printf("%d * %d / %d = %d%n", a, a, a, result1);
